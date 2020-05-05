47ff76f76e8a3f4541876b80ebdf45759d82d21a
abhishek <abhishekdadawal@gmail.com>
2020-05-05 21:25:57 +0530

    UserEntity

--- musichoster-service/src/main/java/com/upgrad/musichoster/service/entity/UserEntity.java
+++ musichoster-service/src/main/java/com/upgrad/musichoster/service/entity/UserEntity.java
@@ -23,124 +23,177 @@ public class UserEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
     @Column(name = "UUID")
     @Size(max = 64)
     private String uuid;
 
+    /**
+     * @return the uuid
+     */
+    public String getUuid() {
+        return uuid;
+    }
+
+    /**
+     * @param uuid the uuid to set
+     */
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
     @Column(name = "ROLE")
     private String role;
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     @Column(name = "EMAIL")
     @NotNull
     @Size(max = 200)
     private String email;
 
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Column(name = "PASSWORD")
     private String password;
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Column(name = "FIRST_NAME")
     @NotNull
     @Size(max = 200)
     private String firstName;
 
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     @Column(name = "LAST_NAME")
     @NotNull
     @Size(max = 200)
     private String lastName;
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Column(name = "MOBILE_PHONE")
     @NotNull
     @Size(max = 50)
     private String mobilePhone;
 
+    /**
+     * @return the mobilePhone
+     */
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+    /**
+     * @param mobilePhone the mobilePhone to set
+     */
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
     @Column(name = "LAST_LOGIN_AT")
     private ZonedDateTime lastLoginAt;
+    /**
+     * @return the lastLoginAt
+     */
+    public ZonedDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+    /**
+     * @param lastLoginAt the lastLoginAt to set
+     */
+    public void setLastLoginAt(ZonedDateTime lastLoginAt) {
+        this.lastLoginAt = lastLoginAt;
+    }
 
     @Column(name = "SALT")
     @NotNull
     @Size(max = 200)
     private String salt;
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMobilePhone() {
-        return mobilePhone;
-    }
-
-    public void setMobilePhone(String mobilePhone) {
-        this.mobilePhone = mobilePhone;
-    }
-
-    public ZonedDateTime getLastLoginAt() {
-        return lastLoginAt;
-    }
-
-    public void setLastLoginAt(ZonedDateTime lastLoginAt) {
-        this.lastLoginAt = lastLoginAt;
-    }
-
+    /**
+     * @return the salt
+     */
     public String getSalt() {
         return salt;
     }
-
+    /**
+     * @param salt the salt to set
+     */
     public void setSalt(String salt) {
         this.salt = salt;
     }
+
 
 
     @Override
